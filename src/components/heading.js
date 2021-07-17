8b91696f0d30aba0c12359d4369105af1ecdab28
sachinmaurya17 <sachin655599@gmail.com>
2021-07-17 11:39:18 +0530

    add heading compnents

---
+++ src/components/heading.js
@@ -0,0 +1,14 @@
+
+import './heading.css';
+
+const  Heading =()=>{
+    return(
+       <>
+        <nav>
+           <p>navbar of the webiste</p>
+        </nav>
+       </>
+    );
+};
+
+export default Heading;
