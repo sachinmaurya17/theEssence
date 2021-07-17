2f964e425f19b742d2a85122df9e8cc6a3e2105e
sachinmaurya17 <sachin655599@gmail.com>
2021-07-17 12:56:43 +0530

    header part

--- src/components/heading.js
+++ src/components/heading.js
@@ -4,11 +4,26 @@ import './heading.css';
 const  Heading =()=>{
     return(
        <>
-        <nav>
-           <p>navbar of the webiste</p>
+        <nav className = "navbar flex">
+           <a className="nav-link">theEssence</a>
+           {/* <button className ="primary-btn">click me</button> */}
+           <ul className="nav-tabs flex">
+               <li className="nav-items">Home</li>
+               <li className="nav-items">Production</li>
+               <li className="nav-items">Services</li>
+               <li className="nav-items">Contact Us</li>
+
+           </ul>
         </nav>
        </>
     );
 };
+
+// js code 
+
+let btn = document.querySelector(".primary-btn");
+console.log(btn)
+
+
 
 export default Heading;
